6729fc2fc0b9cc7db1c8051fb35700fdbc642c3b
Kurachi <naist1020@gmail.com>
2019-07-26 14:37:39 +0900

    add

--- ast_analyze_executor_Nicad.py
+++ ast_analyze_executor_Nicad.py
@@ -39,27 +39,23 @@ if __name__ == '__main__':
     hastest = 0
     nodetect = 0
     count = 0
-    tm_count = 0
-    retm_count = 0
     for i in range(len(NtPath)): 
         Testmethodcalls_list = AstProcessorTestMethodCall(None, BasicInfoListener()).execute('C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/' + NtPath[i]) #target_file_path(テストファイル)内のメソッド名をすべて取得
         Productionmethods_list = AstProcessorProduction(None, BasicInfoListener()).execute(getNicadPath[i]) #プロダクションファイル内のメソッド名をすべて取得
         Testmethods_list = AstProcessorTest(None, BasicInfoListener()).execute('C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/' + NtPath[i]) #target_file_path(テストファイル)内のメソッド呼び出しをすべて取得
 
-        file = open(getNicadPath[i] ,'r')
+        file = open(getNicadPath[i],'r')
         line = file.readline()
         line2 = file.readline()
-      
-        # print('<Nicad Path> ' + getNicadPath[i])
-        # print('<プロダクションPath> ' + line2[2:].replace('\n',''))
-        # print('<テストコードPath> ' + 'C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/' + NtPath[i])
-        # print('<対応するクローンペアのPath> ' + line[2:].replace('\n',''))
-        # print('<テストメソッド>')
-        # # print(Testmethods_list)
-        # for t in Testmethods_list:
-        #     if 'test' in t:
-        #         print(t)
-        #         tm_count += 1
+        print('<Production Code Path> ' + line2[2:].replace('\n',''))
+
+        # print('<プロダクションコードPath>' + getNicadPath[i])
+        print('<Test Code Path> ' + 'C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/' + NtPath[i])
+        print('<Clone Pairs Path> ' + line[2:].replace('\n',''))
+        print('<Test Methods>')
+        # print(Testmethods_list)
+        for t in Testmethods_list:
+            print(t)
 
         cnt = 1
         methodmapcall = defaultdict(list)
@@ -80,48 +76,26 @@ if __name__ == '__main__':
 
         try:
             key = Productionmethods_list[0]
-            # print('<プロダクションメソッド>')
-            # print(key)
-            # print('<再利用候補のテストメソッド>')
-            reusemethods = rd["^(?=.*" + key + ").*$"]
-            # print(rd["^(?=.*" + key + ").*$"])
+            print('<Production Methods>')
+            print(key)
+            print('<Reusable Test Methods>')
+            print(rd["^(?=.*" + key + ").*$"])
             # print(len(rd["^(?=.*" + key + ").*$"]))
-            if len(reusemethods) == 0:
+            if len(rd["^(?=.*" + key + ").*$"]) == 0:
                 notest += 1
-                # print('No Reuse Test')
             else:
                 hastest += 1
-                print('<Production Path> ' + line2[2:].replace('\n',''))
-                print('<Test Code Path> ' + 'C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/' + NtPath[i])
-                print('<Clone Pairs Path> ' + line[2:].replace('\n',''))
-                print('<Test Methods>')
-                # print(Testmethods_list)
-                for t in Testmethods_list:
-                    if 'test' in t:
-                        print(t)
-                        tm_count += 1
 
-                print('<Production Methods>')
-                print(key)
-                print('<Reusable Test Methods>')
-                for j in reusemethods:
-                    print(j[0])
-                # print(len(rd["^(?=.*" + key + ").*$"][0]))
-                    retm_count += 1
-                print('---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
         except IndexError:
-            # print('<プロダクションメソッド>')
+            print('<Production Methods>')
             print('Error')
             nodetect += 1
             pass
         
-        
+        print('---------------------------------------------------------------------------------------------------------------------')
         count += 1
     
-    print('Project Name : ' + projectname)
     print('hastest : ' + str(hastest) + '(' + str(round(hastest/count*100,1)) + ')')
     print('notest : ' + str(notest)  + '(' + str(round(notest/count*100,1)) + ')')
     print('nodetect : ' + str(nodetect)  + '(' + str(round(nodetect/count*100,1)) + ')')
     print('Total : ' + str(count))
-    print('Total Test Methods : ' + str(tm_count))
-    print('Total Reusable Test Methods : ' + str(retm_count))
